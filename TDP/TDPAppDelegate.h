dd02100dbe0c2122f019a5d9c762da3b221d2cb7
Anthony <afigueroa@toptierlabs.com>
2011-11-16 11:48:41 -0200

    Initial Commit

---
+++ TDP/TDPAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  TDPAppDelegate.h
+//  TDP
+//
+//  Created by TopTier on 11/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDPAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
