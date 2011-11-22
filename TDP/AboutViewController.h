4d3ff8f603a3bf0ecabace0fc2f768a8e79513bd
Anthony <afigueroa@toptierlabs.com>
2011-11-22 16:03:49 -0200

    a

--- TDP/AboutViewController.h
+++ TDP/AboutViewController.h
@@ -10,10 +10,10 @@
 #import <CoreData/CoreData.h>
 
 @interface AboutViewController : UIViewController <NSFetchedResultsControllerDelegate> {
-        NSFetchedResultsController *fetchedResultsController;
-        NSManagedObjectContext *managedObjectContext;
+//        NSFetchedResultsController *fetchedResultsController;
+//        NSManagedObjectContext *managedObjectContext;
 }
-
-@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
-@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+//
+//@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+//@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
 @end
